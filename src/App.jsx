bc6733e254e5bd69fa1e5621fbb20314429cd319
Pratap Padekar <unitedpratap7@gmail.com>
2023-04-17 21:39:00 +0530

    Initialized the Project

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import Details from "./pages/Details"
+import Explore from "./pages/Explore"
+import PageNotFound from "./pages/PageNotFound"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+
+
+
+const App = () => {
+  
+  
+  return (
+    <div>
+      {/* <Header /> */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Details />} />
+        <Route path="/" element={<Explore />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+      {/* <Footer /> */}
+    </div>
+  )
+}
+
+export default App
---
+++ src/utils/Endpoints.js
@@ -0,0 +1,7 @@
+
+
+export const Endpoints = {
+    MOVIE_UPCOMING: 'movie/upcoming/'
+}
+
+export const IMAGE_URL = 'https://image.tmdb.org/t/p/original'
